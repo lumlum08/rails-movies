b3b0c8dc1e97164e2b0844a02a1fcd604d8a5d91
georgia drai <djonadre@gmail.com>
2022-01-30 19:30:47 +0100

    creating route review

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: "lists#index"
   resources :lists, only: [:index, :show,  :new, :create] do
     resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create]
   end
   resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
